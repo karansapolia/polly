b9b55ebedd46734c2aada52cfa2a26fc802ed22e
Karan Sapolia <sapoliakaran@gmail.com>
2021-08-18 19:17:16 +0530

    Set results to be an array too in polls_controller

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -37,6 +37,6 @@ class PollsController < ApplicationController
   private
 
     def poll_params
-      params.require(:poll).permit(:title, :results, options: [])
+      params.require(:poll).permit(:title, results: [], options: [])
     end
 end
