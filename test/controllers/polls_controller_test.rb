caf7bbb852bc7939ebb3e4cfe6b10df0d93be1bb
Karan Sapolia <sapoliakaran@gmail.com>
2021-08-24 02:50:39 +0530

    Add polls_controller tests

--- test/controllers/polls_controller_test.rb
+++ test/controllers/polls_controller_test.rb
@@ -4,27 +4,58 @@ require "test_helper"
 
 class PollsControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @poll = Poll.new(title: "This is a test poll", options: "[\"2\",\"3\",\"4\",\"5\"]", results: "[1,2,1,0]")
-    @user_headers = headers(@creator)
+    @user = User.create!(
+      name: "A B",
+      email: "a@b.com",
+      password: "welcome",
+      password_confirmation: "welcome")
+    @unauthorized_user = User.new(
+      name: "Sam Smith",
+      email: "sam@example.com",
+      password: "welcome",
+      password_confirmation: "welcome")
+    @poll = Poll.create!(title: "This is a test poll", options: "[\"2\",\"3\",\"4\",\"5\"]", results: "[1,2,1,0]")
+    @headers = headers(@user)
+    @unauthorized_headers = headers(@unauthorized_user)
   end
 
   def test_should_list_all_polls
-    get tasks_url, headers: @user_headers
-    assert_response :success
-    response_body = response.parsed_body
-    all_tasks = response_body["tasks"]
-
-    pending_tasks_count = Task.where(progress: "pending").count
-    completed_tasks_count = Task.where(progress: "completed").count
-
-    assert_equal all_tasks["pending"].length, pending_tasks_count
-    assert_equal all_tasks["completed"].length, completed_tasks_count
-  end
-
-  def test_should_create_valid_task
-    post tasks_url, params: { task: { title: "Learn Ruby", user_id: @creator.id } }, headers: @creator_headers
+    get polls_url
     assert_response :success
     response_json = response.parsed_body
-    assert_equal response_json["notice"], t("successfully_created", entity: "Task")
+    all_polls = response_json["polls"]
+
+    assert_equal all_polls.length, Poll.count
+  end
+
+  def test_create_poll
+    params = { poll: { title: "A poll", options: ["A", "B", "C", "D"] } }
+    post polls_url, params: params, headers: @headers
+    assert_response :success
+    poll = Poll.find_by(title: "A poll")
+    assert_equal poll.title, params[:poll][:title]
+  end
+
+  def test_should_not_create_poll_for_unauthorized_person
+    params = { poll: { title: "A poll", options: ["A", "B", "C", "D"] } }
+    post polls_url, params: params, headers: @unauthorized_headers
+    assert_response :unauthorized
+    response_json = response.parsed_body
+    assert_equal response_json["errors"], ["Could not authenticate with the provided credentials."]
+  end
+
+  def test_fetch_a_poll
+    get poll_url(1), params: { id: 1 }, headers: @headers
+    assert_response :success
+    response_json = response.parsed_body
+    assert_equal response_json["poll"]["title"], "This is a test poll"
+  end
+
+  def test_update_poll
+    put poll_url(1),
+params: { id: 1, poll: { title: "New Title", options: ["A", "B", "C", "D"], results: [1, 1, 1, 1] } }, headers: @headers
+    assert_response :success
+    response_json = response.parsed_body
+    assert_equal response_json["poll"]["title"], "New Title"
   end
 end
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -3,7 +3,4 @@
 require "test_helper"
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
 end
